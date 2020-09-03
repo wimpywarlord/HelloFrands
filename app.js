b942266f11374e1f68f2f78cff147712febd634a
Kshitij Dhyani <wimpywarlord@gmail.com>
2020-09-03 18:20:16 +0530

    finding the best room

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var express = require("express");
+const { v4: uuidv4 } = require("uuid");
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 express.static("public");
+
+var user_base_schema = new mongoose.Schema({
+	unique_id: String,
+	alone: Boolean,
+	room: String,
+});
+
+var user_base = mongoose.model("user_bases", user_base_schema);
 
 mongoose.connect(
 	"mongodb+srv://wimpywarlord:warlord123@cluster0.0m95r.mongodb.net/<dbname>?retryWrites=true&w=majority",
@@ -24,9 +33,62 @@ app.get("/", function (req, res) {
 });
 
 app.post("/party", function (req, res) {
-	// res.redirect("https://meet.jit.si/xyz");
-	console.log(req.body);
-	// res.redirect("/");
+	var sort = { _id: 1 };
+	var list_of_all_user;
+	var finding_free_person = new Promise(async (resolve, reject) => {
+		await user_base
+			.find({}, function (err, all_users) {
+				if (err) {
+					console.log(
+						"ALL CURRENT USER NOT FETCHED ACCORDING TO TIME STAMP"
+					);
+				} else {
+					console.log("ALL USERS FETCHED");
+					console.log(all_users);
+					list_of_all_user = all_users;
+				}
+			})
+			.sort(sort);
+
+		// console.log("SHOULD PRINT AFTER USER FETCH ");
+
+		// <--CODE FOR FINDING THE BEST ROOM-->
+		var best_free_room;
+		list_of_all_user.forEach((element) => {
+			if (element.alone == true) {
+				best_free_room = element;
+				return false;
+			}
+		});
+		// <--CODE FOR FINDING THE BEST ROOM-->
+
+		if (best_free_room) {
+			// IF WE FIND SOME FREE ROOM
+			console.log(best_free_room);
+		} else {
+			// IF NO FREE ROOM IS AVAILABLE
+			const room_for_current_user = uuidv4();
+			res.redirect(`https://meet.jit.si/${room_for_current_user}`);
+			user_base.create(
+				{
+					unique_id: req.body.unique_id,
+					alone: true,
+					room: room_for_current_user,
+				},
+				function (err, user) {
+					if (err) {
+						console.log("New user NOT created");
+						console.log(err);
+					} else {
+						console.log("New user created");
+						console.log(user);
+					}
+				}
+			);
+		}
+	});
+
+	res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function () {
